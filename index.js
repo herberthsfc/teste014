80ead726a2f12450984042c0defaaac4e446f7e3
herberthsfc <76819767+herberthsfc@users.noreply.github.com>
2021-02-08 15:34:56 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -175,7 +175,8 @@ client.on('group-participants-update', async (anu) => {
 			}
 
 			const botNumber = client.user.jid
-			const ownerNumber = ["5511996237647@s.whatsapp.net"] 
+			const ownerNumber = ["5511996237647@s.whatsapp.net"]
+			const premium = ["5511996237647@s.whatsapp.net"]
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
